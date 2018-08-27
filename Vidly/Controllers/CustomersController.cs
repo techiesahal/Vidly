13466b6b4ea0637f379bf71046e9c59892f0be30
TechieSahal <ts@gmail.com>
2018-08-27 13:23:08 +0530

    AllControllersAndViewCreatedPendingDataInTable

---
+++ Vidly/Controllers/CustomersController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    public class CustomersController : Controller
+    {
+
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // New Customer Action
+        public ActionResult New()
+        {
+            var membershipTypes = _context.MembershipTypes.ToList();
+            var viewModel = new NewCustomerViewModel
+            {
+                MembershipTypes = membershipTypes
+            };
+
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        public ActionResult Create(Customer customer)
+        {
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index", "Customers");
+        }
+
+        // GET: Customers
+        public ActionResult Index() //The return type was ViewResult in the tutorial
+        {
+            var customers = _context.Customers.ToList();
+            //var customers = _context.Customers.Include(c => c.MembershipType).ToList();
+
+            return View(customers);
+        }
+
+        // GET: Customers/Details/1
+        public ActionResult Details(int id)
+        {
+
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            //var customer = _context.Customers.Include(c => c.MembershipType).SingleOrDefault(c => c.Id == id);
+
+            if (customer == null)
+                return Content("There is no customer with such an id: " + id);
+
+            return View(customer);
+        }
+
+    }
+}
---
+++ Vidly/Controllers/MoviesController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using Vidly.Models;
+using Vidly.ViewModels;
+
+namespace Vidly.Controllers
+{
+    //public class MoviesController : Controller
+    //{
+
+    //    // movies
+    //    public ActionResult Index()
+    //    {
+    //        var movies = new List<Movie>
+    //        {
+    //            new Movie {Name = "Shrek!"},
+    //            new Movie {Name = "Wall-e"}
+    //        };
+
+    //        var viewModel = new ListMovieViewModel
+    //        {
+    //            movies = movies
+    //        };
+
+
+    //        return View(viewModel);
+    //    }
+
+
+    //}
+
+    public class MoviesController : Controller
+    {
+        private ApplicationDbContext _context { get; set; }
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        public ViewResult Index()
+        {
+            var movies = _context.Movies.Include(m => m.Genre).ToList();
+
+            return View(movies);
+        }
+
+
+        // GET: Movies/Details/1
+        public ActionResult Details(int id)
+        {
+
+            var movie = _context.Movies.Include(m => m.Genre).SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return Content("There is no movie with such an id: " + id);
+
+            return View(movie);
+        }
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie() { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+    }
+
+}
