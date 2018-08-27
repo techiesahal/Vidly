efcc973d53be3030a84b4163df480d06c8bc9b5a
TechieSahal <ts@gmail.com>
2018-08-27 22:43:27 +0530

    AllWorkingSolutionWithCreate

--- Vidly/Models/Customer.cs
+++ Vidly/Models/Customer.cs
@@ -16,6 +16,7 @@ namespace Vidly.Models
 
         public bool IsSubscribedToNewsLetter { get; set; }
 
+        [Display(Name = "Membership Type")]
         public byte MembershipTypeId { get; set; }
 
         public MembershipType MembershipType { get; set; }
