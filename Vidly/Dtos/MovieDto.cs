745fbe83b44746ddc055af2d170eb65bae11da15
TechieSahal <ts@gmail.com>
2018-09-08 07:16:42 +0530

    APICompleted

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+        [Range(1, 20)]
+        public byte NumberInStock { get; set; }
+
+        public int GenreId { get; set; }
+
+    }
+}
